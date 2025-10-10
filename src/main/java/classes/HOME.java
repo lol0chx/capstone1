634ad335dc8bcb3d1340accaf219233f2bdab399
lol0chx <henoksmekuria@gmail.com>
2025-10-10 12:46:22 -0700

    start

---
+++ src/main/java/classes/HOME.java
@@ -0,0 +1,9 @@
+package classes;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HOME {
+    static void main() {
+        System.out.println("TEST");
+    }
+}
